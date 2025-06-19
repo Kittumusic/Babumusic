932252a2b28c790d68c6d52ef07daba061e97337
Kittumusic <rajababutg825@gmail.com>
2025-06-19 22:14:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RAJA-YT/NYSAMUSIC",
+    "https://github.com/Kittumusic/Babumusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
